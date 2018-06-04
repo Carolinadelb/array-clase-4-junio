86cbc104a4beceefff31be3711a945b5f2ddcdce
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-04 10:14:41 -0400

    primera oarte de la clase

---
+++ app.js
@@ -0,0 +1,33 @@
+let names = [];
+//let names = new Array;
+names [0] = 'Paula'
+//Cambia un valor... si ya existe un elemento en la posición 1:
+names [1] = 'Majo'
+
+        //PUSH
+
+//Otra manera de agregar elementos(datos) al final de mi arreglo: 
+names.push ('Majo', 'teresita', 20, true, false)
+
+
+      //POP
+//Quitar (siempre) el último elemento:
+name.pop()
+
+      //SLICE
+
+//  slice (quiero q me copies desde eñl indice tanto al indice tanto).
+// número negativo hace correr desde atrás hacia adelante.
+
+const names = ['ale', 'aggretsuko', 'hello kitty', 'violeta', 'caro', 'otronombre'];
+//para hacer slice
+// names.slice(0, 4), tomara desde ale hasta violeta porque no toma el último valor 4  (0 si la incluye)
+
+// si solo pongo:
+//name.slice(2)  ira desde comienza la posición q pide el n°2 hasta el final
+
+      //INDEX OF
+
+
+const newNames = names.slice(2) //hará que la nueva variable sea del arreglo del nuevo slice.
+
